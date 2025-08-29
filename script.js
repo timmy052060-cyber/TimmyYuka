47bf7f0d2dd47e87387b31df3f7c7248928af3a6
timmy052060-cyber <timmy052060@gmail.com>
2025-08-29 16:14:14 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const NavMenu = document.querySelector(".nav_menu")
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     NavMenu.classList.toggle("active");
-
 })
+
+document.querySelectorAll(".nav_link").forEach(n => n.
+    addEventListener("click"), () => {
+        hamburger.classList.remove("active");
+        NavMenu.classList.remove("active");
+    }
+)
